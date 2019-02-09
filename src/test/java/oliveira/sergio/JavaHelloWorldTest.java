66e9f4b69a12e0fff9bb395589cd0e1d21de2539
sergiopoliveira <sergiopoliveira@outlook.com>
2019-02-09 14:35:27 +0000

    junit4

---
+++ src/test/java/oliveira/sergio/JavaHelloWorldTest.java
@@ -0,0 +1,15 @@
+package oliveira.sergio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello(){
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+
+}
