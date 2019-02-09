482de6076593902dc1b655c1b25ada92a7e633db
sergiopoliveira <sergiopoliveira@outlook.com>
2019-02-09 15:25:05 +0000

    Spock

---
+++ src/test/groovy/oliveira/sergio/JavaHelloWorldSpecTest.groovy
@@ -0,0 +1,14 @@
+package oliveira.sergio
+
+import spock.lang.Specification
+
+class JavaHelloWorldSpecTest extends Specification {
+    def "GetHello"() {
+
+        when:
+        def foo = "123"
+
+        then:
+        true
+    }
+}
