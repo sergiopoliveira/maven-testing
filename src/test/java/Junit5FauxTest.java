dd2d2a9fa1db5daed4826507c0abcf111335db1a
sergiopoliveira <sergiopoliveira@outlook.com>
2019-02-09 14:52:00 +0000

    Junit 4 and Junit5

--- src/test/java/Junit5FauxTest.java
+++ src/test/java/Junit5FauxTest.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
 
 public class Junit5FauxTest {
+
+
+    @Test
+    void someTestForJunit5(){
+        System.out.println("I Ran...");
+    }
 }
